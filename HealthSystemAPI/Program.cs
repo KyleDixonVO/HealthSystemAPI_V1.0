6bb47c5ea82db835f8e6f1e166ac4c6bdcb2bf4a
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-18 12:10:19 -0300

    Initial Upload

---
+++ HealthSystemAPI/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystemAPI
+{
+    class Program
+    {
+        static int health = 100;
+        static int shield = 100;
+        static int lives = 3;
+        static int damage;
+        static int hp;
+        
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey(true);
+        }
+
+        static void ShowHUD()
+        {
+
+
+        }
+
+        static void TakeDamage(int damage)
+        {
+
+
+        }
+
+        static void Heal( int hp)
+        {
+
+
+        }
+
+        static void RegenerateShield(int hp)
+        {
+
+
+        }
+    }
+}
