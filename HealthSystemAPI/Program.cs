b6183cff3136e46c85b5cd8aa7729e0e95440db6
Kyle Dixon <90637263+KyleDixonVO@users.noreply.github.com>
2021-10-19 18:47:50 -0300

    Finished Heal, SetHPVariable, HealRNG, DamageRNG, ShowHUD

--- HealthSystemAPI/Program.cs
+++ HealthSystemAPI/Program.cs
@@ -13,11 +13,16 @@ namespace HealthSystemAPI
         static int lives = 3;
         static int damage;
         static int hp;
-        
+        static bool gameOver;
+        static string howBloodied;
         static void Main(string[] args)
         {
-
-
+            while (gameOver == false)
+            {
+                ShowHUD();
+                SetHPVariable();
+                Heal(hp);
+            }
 
 
 
@@ -48,7 +53,13 @@ namespace HealthSystemAPI
 
         static void ShowHUD()
         {
-
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Health: " + health + "%");
+            HowCloseToDead();
+            Console.WriteLine(howBloodied);
+            Console.WriteLine("-------------------------------");
 
         }
 
@@ -61,30 +72,83 @@ namespace HealthSystemAPI
                 shield = shield - damage;
 
             }
-            else if (health >0)
+            else if (health > 0 && shield <= 0)
             {
                 health = health + shield - damage;
                 shield = 0;
             }
-            else if (health <=0)
+            else if (health <= 0)
             {
                 lives--;
-                if (lives >0)
+                if (lives > 0)
                 {
-                    health = 100;
+                    Respawn();
+                }
+                else
+                {
+                    gameOver = true;
                 }
             }
         }
 
-        static void RngMachine()
+        static void Respawn()
+        {
+            health = 100;
+            shield = 100;
+        }
+
+        static void DamageRNG()
         {
             Random rd = new Random();
-            damage = rd.Next(15, 75); 
+            damage = rd.Next(40, 60);
+        }
+
+        static void HealRNG()
+        {
+            int[] healingAmount = new int[3];
+            healingAmount[0] = 25;
+            healingAmount[1] = 50;
+            healingAmount[2] = 100;
+
+            Random rd2 = new Random();
+            int whichHeal = rd2.Next(0, 2);
+            healingAmount[whichHeal] = hp;
+        }
+
+        static void SetHPVariable()
+        {
+            Console.WriteLine("Enter the value of hp. (integer)");
+            string input = Console.ReadLine();
+            bool isInteger = int.TryParse(input, out int result);
+            if (isInteger == true)
+            {
+                hp = result;
+                Console.WriteLine("Set hp to: " + hp);
+            }
+            else
+            {
+                Console.WriteLine("****Invalid Input: input must be an integer!****");
+                SetHPVariable();
+            }
         }
 
         static void Heal(int hp)
         {
+            if (hp >= 0)
+            {
+                Console.WriteLine("Player is about to be healed for: " + hp + " hitpoints.");
+                health = health + hp;
+            }
+            else
+            {
+                Console.WriteLine("****The value of 'hp' must be zero, or a positive integer!****");
+            }
 
+            if(health > 100)
+            {
+                Console.WriteLine("Player cannot be further healed.");
+                health = 100;
+            }  
 
         }
 
@@ -114,8 +178,17 @@ namespace HealthSystemAPI
         }
         static void RegenerateShield(int hp)
         {
+            shield = shield + hp;
 
 
+        }
+
+        static void Reset()
+        {
+            health = 100;
+            shield = 100;
+            lives = 3;
+            Console.WriteLine("****Game Reset.****");
         }
     }
 }
