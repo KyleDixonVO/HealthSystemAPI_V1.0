4133692386ec14a7b452e18dbea4b26f1a0a7690
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-19 16:28:14 -0300

    Fleshed out Damage, added HowCloseToDead

--- HealthSystemAPI/Program.cs
+++ HealthSystemAPI/Program.cs
@@ -54,16 +54,64 @@ namespace HealthSystemAPI
 
         static void TakeDamage(int damage)
         {
+            Console.WriteLine("Player Is About To Take: " + damage + " damage");
 
+            if (shield > 0)
+            {
+                shield = shield - damage;
 
+            }
+            else if (health >0)
+            {
+                health = health + shield - damage;
+                shield = 0;
+            }
+            else if (health <=0)
+            {
+                lives--;
+                if (lives >0)
+                {
+                    health = 100;
+                }
+            }
         }
 
-        static void Heal( int hp)
+        static void RngMachine()
+        {
+            Random rd = new Random();
+            damage = rd.Next(15, 75); 
+        }
+
+        static void Heal(int hp)
         {
 
 
         }
 
+        static void HowCloseToDead()
+        {
+            if (health > 0 && health < 10)
+            {
+                howBloodied = "Death's Door";
+            }
+            else if (health >= 10 && health < 50)
+            {
+                howBloodied = "That's a Lot of Blood";
+            }
+            else if (health >= 50 && health < 75)
+            {
+                howBloodied = "Only a Flesh Wound";
+            }
+            else if (health >= 75 && health < 100)
+            {
+                howBloodied = "Tis But a Scratch";
+            }
+            else if (health >= 100)
+            {
+                health = 100;
+                howBloodied = "Flawless Health";
+            }
+        }
         static void RegenerateShield(int hp)
         {
 
